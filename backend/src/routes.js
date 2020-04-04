6f2c593d6ff5bc5f04b939715721d9457b7f1904
Tiago Meurer <tiagomeurersc@gmail.com>
2020-04-03 23:40:36 -0300

    implementacao de rotas no backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,23 +1,18 @@
 const express = require('express');
-const crypto = require('crypto');
 
-const connection = require('./database/connection');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+
 const routes = express.Router();
 
-routes.post('/ongs', async (request, response) => {
-    const { name, email, whatsapp, city, uf } = request.body;
-    const id = crypto.randomBytes(4).toString('HEX');
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-    await connection ('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-    })
-    
-    return response.json({ id }); 
-});
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete)
 
 module.exports = routes;
